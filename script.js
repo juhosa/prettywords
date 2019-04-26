35fbc255473eaeeaf9998443d135c4e95ec9508f
Juho Salli <juho.salli@gmail.com>
2019-04-26 14:56:51 +0300

    Creates random balls in random places with random colors

---
+++ script.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', ready, false)
+
+let canvas = null
+let ctx = null
+
+let windowWidth = null
+let windowHeight = null
+
+let colors = [
+    "84DCC6",
+    "A5FFD6",
+    "FFA69E",
+    "FF686B"
+]
+
+let radiuses = [100, 150, 175, 200, 250]
+
+function randomColor () {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function randomRadius () {
+    return radiuses[Math.floor(Math.random() * radiuses.length)]
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function ready () {
+    console.log('on ready()')
+    // console.dir(document.getElementById('the_canvas'))
+    windowWidth = window.innerWidth
+    windowHeight = window.innerHeight
+
+    canvas = document.getElementById('the_canvas')
+    ctx = canvas.getContext('2d')
+
+    canvas.width = windowWidth
+    canvas.height = windowHeight
+
+    let words = ['bat', 'fd', 'pipenv']
+
+    for (let w of words) {
+        createCircle(w)
+    }
+
+    drawCenterDot()
+
+}
+
+function drawCenterDot () {
+    let r = 5
+    let x = windowWidth / 2
+    let y = windowHeight / 2
+
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, 2 * Math.PI)
+    // ctx.stroke()
+    ctx.fillStyle = '#000000'
+    ctx.fill()
+}
+
+function createCircle (word) {
+    let r = randomRadius()
+    let x = randomInt(r, windowWidth - r)
+    let y = randomInt(r, windowHeight - r)
+
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, 2 * Math.PI)
+    // ctx.stroke()
+    ctx.fillStyle = '#' + randomColor()
+    ctx.fill()
+
+    ctx.font = '40pt Verdana'
+    ctx.fillStyle = 'white'
+    ctx.textAlign = 'center'
+    ctx.fillText(word, x, y + 20)
+}
